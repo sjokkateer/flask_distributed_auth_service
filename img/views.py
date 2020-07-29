9a081ba611e25c08ce16797cf9e974287f778672
sjokkateer <remy90@hotmail.com>
2020-07-29 12:53:31 +0200

    IMAGE SERVICE: Add endpoint to get 10 recent uploaded images

--- img/views.py
+++ img/views.py
@@ -1,6 +1,7 @@
 from app import db
 from flask import jsonify, request
 from flask.views import MethodView
+from models import Image
 from schemas import ImageSchema
 
 import jwt
@@ -9,24 +10,38 @@ import requests
 
 
 image_schema = ImageSchema()
+images_schema = ImageSchema(many=True)
+
+algorithms='RS256'
 
 
 class ImageUploadView(MethodView):
-    def post(self):
-        # verify user's access token by getting
-        # the required key by id from auth api
+    def get(self):
+        '''
+        Endpoint returns the images for user with user_id
+        '''
         access_token = get_access_token(request)
-        payload = jwt.decode(access_token, verify=False)
-        
-        key_id = payload.get('key_id')
-        url = f"{os.getenv('AUTH_SERVER')}/keys/{key_id}"
-        
-        response = requests.get(url)
-        pub_key = response.json()[f'{key_id}']
+        public_key = get_public_key(access_token)
 
-        # Crashes on uploading when trying to verify an expired access_token
         try:
-            payload = jwt.decode(access_token, pub_key, algorithms='RS256')
+            payload = jwt.decode(access_token, public_key, algorithms=algorithms)
+        except jwt.exceptions.ExpiredSignatureError as e:
+            return jsonify({'message': e.args[0]}), 401
+
+        user_id = payload.get('user_id')
+        images = Image.query.filter_by(user_id=user_id).order_by(Image.uploaded_at.desc()).limit(10).all()
+
+        return jsonify({'images': images_schema.dump(images)})
+
+    def post(self):
+        '''
+        Endpoint for user with user_id to upload new images
+        '''
+        access_token = get_access_token(request)
+        public_key = get_public_key(access_token)
+
+        try:
+            payload = jwt.decode(access_token, public_key, algorithms=algorithms)
         except jwt.exceptions.ExpiredSignatureError as e:
             return jsonify({'message': e.args[0]}), 401
         
@@ -40,14 +55,20 @@ class ImageUploadView(MethodView):
             db.session.commit()
 
             return jsonify({'image': image_schema.dump(image)})
-        # Check user's id in claim vs the posted id
-        # if that matches, validate the image
 
-        # if that checks out, store the image.
-        return jsonify({'message': 'Alive!'})
+        return jsonify({'message': 'Something went wrong'})
 
 
 def get_access_token(request):
     bearer_token = request.headers.get('Authorization')
     # Of course should do some checks normally but lets keep it simple for now
     return bearer_token.split()[-1]
+
+def get_public_key(access_token):
+    payload = jwt.decode(access_token, verify=False)
+        
+    key_id = payload.get('key_id')
+    url = f"{os.getenv('AUTH_SERVER')}/keys/{key_id}"
+        
+    response = requests.get(url)
+    return response.json()[f'{key_id}']
